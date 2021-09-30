a8da0281ff9b24f4f4213fd235c8572f8e5ad76b
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2021-09-30 14:19:15 -0300

    ex3 - minha solucao

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int d = scan.nextInt();
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		int diferenca = (a*b) - (c*d);
+		System.out.println(diferenca);
 		
-		double R, A, pi = 3.14159;
-	    
-	    R = sc.nextDouble();
-
-	    A = pi * R * R;
-
-	    System.out.printf("A=%.4f%n", A);
-
-		sc.close();
 	}
+
 }
