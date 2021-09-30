bfd38f2f67cf61940e69d681e3406cc0808747dc
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2021-09-30 14:08:45 -0300

    ex2 - minha solucao

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
+		double x = 0;
+		double raio = 0;
 		Scanner sc = new Scanner(System.in);
-
-		int A, B, soma;
-
-		A = sc.nextInt();
-		B = sc.nextInt();
-
-		soma = A + B;
-
-		System.out.println("SOMA = " + soma);
-
+		
+		x = sc.nextDouble();
+		
+		raio = Math.PI * Math.pow(x, 2);
+		System.out.printf("o raio e: %.4f%n", raio);
+		
+		
+		
+		
 		sc.close();
 	}
 }
