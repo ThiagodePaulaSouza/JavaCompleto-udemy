cdc2ed7d53e9b9dc23dfc89f520b0537462afca5
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2021-09-30 14:20:10 -0300

    ex3 - solucao desejada

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		int d = scan.nextInt();
-		
-		int diferenca = (a*b) - (c*d);
-		System.out.println(diferenca);
-		
-	}
 
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, C, D, dif;
+
+	    A = sc.nextInt();
+	    B = sc.nextInt();
+	    C = sc.nextInt();
+	    D = sc.nextInt();
+
+	    dif = A * B - C * D;
+
+	    System.out.println("DIFERENCA = " + dif);	
+		
+		sc.close();
+	}
 }
