edb27bd105e97ad060234cddff197c9bed6b8247
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2021-09-30 13:53:06 -0300

    ex1 - minha solucao

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		int x = 0;
+		int y = 0;
+		Scanner sc = new Scanner(System.in);
 		
-		String teste = "era uma vez um cachorro que latia muito porem houve um dia que ele latiu tao alto que morreu";
+		System.out.println("digite dois numeros: ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		sc.close();
 		
-		System.out.println(teste.replaceAll("[aeiou]", ""));
+		System.out.println("a soma e: " + soma(x,y));
+	}
+	
+	public static int soma(int x, int y) {
+		return x+y;
 	}
 }
